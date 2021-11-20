de1e6fbabf59c1f25f2a57e6fd5f477e74cf01da
Sodai-Komichi <83051052+Aso2101122@users.noreply.github.com>
2021-11-20 19:03:56 +0900

    dbmanager修正

--- 07_ソースコード/dbmanager.php
+++ 07_ソースコード/dbmanager.php
@@ -2,11 +2,11 @@
 function getDb()
 {
     // データベースへの接続を確認
-    $dsn = 'mysql:host=mysql153.phy.lolipop.lan;
-    dbname=LAA1290588-school;
+    $dsn = 'mysql:host=mysql152.phy.lolipop.lan;
+    dbname=LAA1290588-coffeeec;
     charset=utf8';
     $username = 'LAA1290588';
-    $password = 'phpdb5312';
+    $password = 'Aso5han';
     $pdo = new PDO($dsn, $username, $password);
     return $pdo;
 }

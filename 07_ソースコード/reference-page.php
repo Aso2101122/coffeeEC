37d20e389fcdd9e1c477d8a6f4f055b79b2fd2ed
宮田　湧人 <2001041@s.asojuku.ac.jp>
2021-12-14 14:33:44 +0900

    12/14(reference2)

--- 07_ソースコード/reference-page.php
+++ 07_ソースコード/reference-page.php
@@ -24,20 +24,24 @@ session_start();
     <div class="main-content">
         <h1 class="recommendation-reference"><span>おすすめ解説サイト</span></h1>
         <div class="reference-page1">
-            <img src="img/reference_page_img1.jpg" class="reference-img1"><br>
-            <a href="https://every-coffee.com/article/coffee-item.html">コーヒー初心者でも自宅で手軽にできる簡単なコーヒーの入れ方</a>
+            <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/68rTkR7qabk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <br>
+            <a class="text1">【初心者向け】美味しいコーヒーの淹れ方</a>
         </div>
         <div class="reference-page2"> 
-            <img src="img/reference_page_img2.jpg" class="reference-img2"><br>
-            <a href="https://cafend.net/teastrainer-coffee/">ドリッパーもフィルターも不要!?茶こしを使ってコーヒーをおいしく淹れる方法</a>
+            <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/3OZkPfPqH-A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <br>
+            <a class="text2">キッチン用品だけでコーヒーを淹れる方法</a>
         </div>
         <div class="reference-page3">
-            <img src="img/reference_page_img3.jpg" class="reference-img3"><br>
-            <a href="https://cafict.com/make-coffee/coffee-tools/first-coffeetools/">一番美味しいコーヒーの入れ方】プロのハンドドリップ動画11選</a>
+            <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/o3eMg4DYLKo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <br>
+            <a class="text3">世界一美味しいコーヒーの淹れ方</a>
         </div>
         <div class="reference-page4">
-            <img src="img/reference_page_img4.jpg" class="reference-img4"><br>
-            <a href="https://every-coffee.com/article/coffee-drip.html">おうちで本格ハンドドリップコーヒーを 始めるために揃えるべき7つの道具。</a>
+            <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/FNDnh7Zo7Ec" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <br>
+            <a class="text4">最初に買うべき4つのマストアイテム</a>
         </div>
     </div>  
 </body>
